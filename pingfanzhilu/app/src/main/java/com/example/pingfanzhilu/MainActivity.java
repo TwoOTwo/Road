a1cf8a00b7e01811ca46de6f02542e390cf94529
yangzhiming <739834798@qq.com>
2015-10-15 15:47:36 +0800

    总框架设计

--- pingfanzhilu/app/src/main/java/com/example/pingfanzhilu/MainActivity.java
+++ pingfanzhilu/app/src/main/java/com/example/pingfanzhilu/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
@@ -110,11 +111,46 @@ public class MainActivity extends Activity {
                     startActivity(intent5);
                 }
             });
+            ImageButton daikaifa = (ImageButton)findViewById(R.id.ibu6);
+            daikaifa.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "此功能开发中",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
             ImageButton gongyijiaodian = (ImageButton)findViewById(R.id.ibu12);
             gongyijiaodian.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     Intent intent6 = new Intent(MainActivity.this, gongyijiaodian.class);
                     startActivity(intent6);
+                }
+            });
+            ImageButton renwuzhongxin = (ImageButton)findViewById(R.id.ibu13);
+            renwuzhongxin.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent7 = new Intent(MainActivity.this, renwuzhongxin.class);
+                    startActivity(intent7);
+                }
+            });
+            ImageButton hezuojigou = (ImageButton)findViewById(R.id.ibu14);
+            hezuojigou.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent8 = new Intent(MainActivity.this, hezuojigou.class);
+                    startActivity(intent8);
+                }
+            });
+            ImageButton aixinluntan = (ImageButton)findViewById(R.id.ibu15);
+            aixinluntan.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent9 = new Intent(MainActivity.this, aixinluntan.class);
+                    startActivity(intent9);
+                }
+            });
+            ImageButton wangqitupian = (ImageButton)findViewById(R.id.ibu16);
+            wangqitupian.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent10 = new Intent(MainActivity.this, wangqitupian.class);
+                    startActivity(intent10);
                 }
             });
             ImageButton titleMe = (ImageButton) findViewById(R.id.ibu11);
@@ -125,7 +161,21 @@ public class MainActivity extends Activity {
                             Toast.LENGTH_SHORT).show();
                 }
             });
-
+            Button sousuo = (Button) findViewById(R.id.bu1);
+            sousuo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "你好，没完善此功能",
+                            Toast.LENGTH_SHORT).show();
+                }
+            });
+            ImageButton gerenzhongxin = (ImageButton)findViewById(R.id.ibu11);
+            gerenzhongxin.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    Intent intent11 = new Intent(MainActivity.this, gerenzhongxin.class);
+                    startActivity(intent11);
+                }
+            });
         }
     };;
 }
---
+++ pingfanzhilu/app/src/main/java/com/example/pingfanzhilu/gerenzhongxin.java
@@ -0,0 +1,26 @@
+package com.example.pingfanzhilu;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+/**
+ * Created by 杨~ on 2015/10/15.
+ */
+public class gerenzhongxin extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.gerenzhongxinactivity);
+        Button denglu = (Button) findViewById(R.id.button);
+        denglu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent12 = new Intent(gerenzhongxin.this,denglu.class);
+                startActivity(intent12);
+            }
+        });
+    }
+}
